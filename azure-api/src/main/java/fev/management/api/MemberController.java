be5b1aa08c2bd819f32cfcfdfc3f6ec7971e4bb6
nguyenbachtoan <nguyenbachtoan120997@gmail.com>
2019-07-17 12:05:19 +0700

    feat: added updated member

--- azure-api/src/main/java/fev/management/api/MemberController.java
+++ azure-api/src/main/java/fev/management/api/MemberController.java
@@ -18,19 +18,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import fev.management.repository.MemberRepository;
-
+@CrossOrigin
 @Controller
 public class MemberController implements BaseController<FevMember, FevMemberModel> {
 
@@ -50,7 +41,7 @@ public class MemberController implements BaseController<FevMember, FevMemberMode
     @Autowired
     MemberPositionRepository memberPositionRepository;
 
-    @CrossOrigin
+
     @GetMapping(path = path)
     @ResponseBody
     @Override
@@ -70,9 +61,9 @@ public class MemberController implements BaseController<FevMember, FevMemberMode
     }
 
     // Find members by name
-    @PostMapping(path + "/name")
+    @GetMapping(path + "/name")
     @ResponseBody
-    public List<FevMember> getByName(@Valid @RequestBody String name) {
+    public List<FevMember> getByName(@RequestParam String name) {
         // TODO Auto-generated method stub
         return memberRepository.findByName(name);
     }
--- azure-api/src/main/java/fev/management/api/MemberPositionController.java
+++ azure-api/src/main/java/fev/management/api/MemberPositionController.java
@@ -8,18 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import fev.management.entity.FevAccount;
 import fev.management.entity.FevMember;
 import fev.management.entity.FevMemberPosition;
 import fev.management.repository.MemberPositionRepository;
+@CrossOrigin
 
 @Controller
 public class MemberPositionController implements BaseController<FevMemberPosition,FevMemberPosition> {
